f9318daacee33269a54d250b0ffa5082229d60c0
EjikeOdoh <ejike.developer@gmail.com>
2025-06-25 15:57:29 +0100

    Connect pg database from render

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,39 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { StudentsModule } from './students/students.module';
 import { ProgramsModule } from './programs/programs.module';
 import { GradesModule } from './grades/grades.module';
 import { ParticipationModule } from './participation/participation.module';
 
 @Module({
-  imports: [StudentsModule, ProgramsModule, GradesModule, ParticipationModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true, 
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }),
+    }),
+    StudentsModule,
+    ProgramsModule,
+    GradesModule,
+    ParticipationModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
