5629467b1dcd2682647e759ef21d066e192dcbc2
EjikeOdoh <ejike.developer@gmail.com>
2025-07-03 09:27:36 +0100

    improve auth guard

--- src/auth/guard/auth.guard.ts
+++ src/auth/guard/auth.guard.ts
@@ -5,7 +5,6 @@ import {
     UnauthorizedException,
   } from '@nestjs/common';
   import { JwtService } from '@nestjs/jwt';
-  import { jwtConstants } from '../constants';
   import { Request } from 'express';
 import { Reflector } from '@nestjs/core';
 import { IS_PUBLIC_KEY } from 'src/decorators/decorators';
